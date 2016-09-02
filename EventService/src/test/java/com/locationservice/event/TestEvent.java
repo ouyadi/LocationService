c5b33910855b6b60b11a1f64bc2f4161309a5aa9
ouyadi <ouyadi@DESKTOP-LJ9ARDT>
2016-09-01 21:24:56 -0400

    adding junit class

---
+++ EventService/src/test/java/com/locationservice/event/TestEvent.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.locationservice.event;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ouyadi
+ *
+ */
+public class TestEvent {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
